dc00f3b2e5d081d6189cccf08b48ed18bf638850
Diego Pino Navarro <dpino@metro.org>
2020-02-19 21:13:06 -0500

    Use JS provided by Marlo
    heads up to @marlo-longley. This was coded by him

--- js/global.js
+++ js/global.js
@@ -5,18 +5,28 @@
  */
 (function ($, Drupal) {
 
-  'use strict';
+    'use strict';
 
-  Drupal.behaviors.archipelago_subtheme = {
-    attach: function (context, settings) {
-        $(window).on('resize', function() {
-          // de-collapses if collapsed and media query restores large viewport.
-          var MOBILE_WIDTH = 992;
-            if ($(window).width() >= MOBILE_WIDTH) {
-              $('.navbar-collapse.collapse.show').removeClass('show');
+    Drupal.behaviors.esie_subtheme = {
+        attach: function (context, settings) {
+            $(window).on('resize', function() {
+                // de-collapses if collapsed and media query restores large viewport.
+                var MOBILE_WIDTH = 992;
+                if ($(window).width() >= MOBILE_WIDTH) {
+                    $('.navbar-collapse.collapse.show').removeClass('show');
+                }
+            });
+
+            // Triggers admin toolbar offset on page load
+            if (context === document) {
+                $(context).on("drupalViewportOffsetChange.toolbar", function (event, offsets) {
+                    let $body = $("body");
+                    if ($body.length > 0 && offsets.top > 0 && $body.css('padding-top') !== offsets.top) {
+                        $body.css("padding-top", offsets.top);
+                    }
+                });
             }
-        });
+        }
     }
-  }
 
 })(jQuery, Drupal);
